1d0ab2f3f9768da2eeabd0bc74cd7912219bc048
Fajardo <fajardonaranjoja@gmail.com>
2023-10-11 19:52:57 +0200

    Fin de la actividad 11.AcertarNumeros

--- 11.AcertarNumeros/WEB-INF/classes/AcertarNumeros.java
+++ 11.AcertarNumeros/WEB-INF/classes/AcertarNumeros.java
@@ -14,44 +14,89 @@ public class AcertarNumeros extends HttpServlet{
 	
 	@Override
 	public void init() throws ServletException {
+	
 		String numerosParaTratar = getServletConfig().getInitParameter("numeros");
+		
 		String[] numerosSplit = numerosParaTratar.split(";");
+		
+		// Esto podría saltarse
 		List<String> listaNumerosAux = Arrays.asList(numerosSplit);
+		
 		ArrayList<String> listaNumeros = new ArrayList<String>(listaNumerosAux);
 		
 		getServletContext().setAttribute("listaNumeros", listaNumeros);
-		getServletContext().setAttribute("mapaPremios", new HashMap(String, Integer));
+		
+		getServletContext().setAttribute("mapaPremios", new HashMap<String, Integer>());
+		
 	}
 	
 	
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
 		
-		response.setContentType("text/html;charset=UTF-8"); 
+		response.setContentType("text/html"); 
 		PrintWriter out = response.getWriter(); 
 		
 		String nombreUsuario = request.getParameter("usuario");
 		String intentoNumero = request.getParameter("numeroIntroducido");
 		
-		
-		
 		ServletContext contexto = getServletContext();
 		ArrayList<String> numeros = (ArrayList<String>)contexto.getAttribute("listaNumeros");
+		HashMap<String, Integer> mapaGanadores = (HashMap<String, Integer>)contexto.getAttribute("mapaPremios");
 		
 		out.println("<html>");
 		out.println("<head>");
 		out.println("<link rel='stylesheet' href='styles/style.css'> ");
-		
 		out.println("</head>");
 		out.println("<body>");
-		for (String numero : numeros)
-			out.println(numero + " ");
 		out.println("<article>");
+		
+		if (!numeros.isEmpty()) {
+		
+			if (numeros.contains(intentoNumero)){
 				
-			
+				if (mapaGanadores.containsKey(nombreUsuario)) {
+					Integer vecesGanadas = mapaGanadores.get(nombreUsuario);
+					vecesGanadas++;
+					mapaGanadores.put(nombreUsuario, vecesGanadas);
+				} else {
+					mapaGanadores.put(nombreUsuario, 1);
+				}
+				
+				numeros.remove(intentoNumero);
+				
+				if (numeros.size() == 0)
+					out.println("<h1>Felicidades "+nombreUsuario+", has acertado el último número</h1>");
+				else out.println("<h1>Felicidades "+nombreUsuario+", quedan "+numeros.size()+" aún, suerte!</h1>");
+				
+				
+				for (Map.Entry<String, Integer> entrada : mapaGanadores.entrySet()){
+					out.println(entrada.getKey() + " | nº victorias: "+ entrada.getValue()+"<br>");
+				}
+				
+			} else {
+				out.println("<h1>Lo sentimos "+nombreUsuario+", ha fallado, quedan "+numeros.size()+" aún, suerte!</h1>");
+			}
+		
+		} else {
+		
+			out.println("<h1>Han terminado los números "+nombreUsuario+", nos vemos</h1>");
+			out.println("<h2>Lista de ganadores:</h2>");
+			for (Map.Entry<String, Integer> entrada : mapaGanadores.entrySet()){
+					out.println(entrada.getKey() + " | nº victorias: "+ entrada.getValue()+"<br>");
+			}
+		
+		}
+		
+		out.println("<form action='index.html'>");
+		out.println("<input type='submit' value='Volver' name='boton'>");
+		out.println("</form>");
+		
+		
 		out.println("</article>");
 		
-		out.println("</body>");
+
 		
+		out.println("</body>");
 		out.println("</html>");
 		
 	}
