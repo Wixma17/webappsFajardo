fcf0db969886b19c7a54cb2f3b939b774b3191e9
Fajardo <fajardonaranjoja@gmail.com>
2023-10-17 22:01:21 +0200

    Terminado el ejercicio 13, falta el comprobar que existe una sesión

--- 12A.AmortizacionesPrestamoForward/WEB-INF/classes/PintarTabla.java
+++ 12A.AmortizacionesPrestamoForward/WEB-INF/classes/PintarTabla.java
@@ -90,7 +90,8 @@ public class PintarTabla extends HttpServlet{
 			out.println("<td colspan='2'>Total capital amortizado: "+df.format(sumaCapitalAmortizado)+"</td>");
 			out.println("</tr>");
 			
-			
+			sumaInteres = 0;
+			sumaCapitalAmortizado = 0;
 			
 			out.println("</table>");
 			
